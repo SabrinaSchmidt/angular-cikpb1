402325327f4456eef8f79bf79685e5c59fefdbbb
SabrinaSchmidt <sabrinaschmidt335@gmail.com>
2021-06-07 10:49:22 -0300

    4 files modified

--- src/app/product-alerts/product-alerts.component.ts
+++ src/app/product-alerts/product-alerts.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Input } from '@angular/core';
-import { Product } from '../products';
-
+import { Output, EventEmitter } from '@angular/core';
+import { Product, products } from '../products';
 @Component({
   selector: 'app-product-alerts',
   templateUrl: './product-alerts.component.html',
@@ -9,10 +9,21 @@ import { Product } from '../products';
 })
 
 export class ProductAlertsComponent implements OnInit {
-  @Input() product!: Product;
+   @Input() product: Product|undefined;
+  @Output() notify = new EventEmitter();
   constructor() { }
 
   ngOnInit() {
   }
+}
+export class ProductListComponent {
+  products = products;
 
+  share() {
+    window.alert('The product has been shared!');
+  }
+
+  onNotify() {
+    window.alert('You will be notified when the product goes on sale');
+  }
 }
